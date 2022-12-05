1b3debd880392ef3773804aae9e1f6464b445a1b
juaniarrigoni <juani.arrigoni@gmail.comgit config --global user.name juaniarrigonigit initgit config --global user.email juani.arrigoni@gmail.com>
2022-12-05 12:54:18 -0300

    Empezando con la navbar

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+export default function Navbar() {
+  return <></>;
+}
