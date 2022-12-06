a409e384eb6eb16387e1831b08062a38dd127495
Santiago L. Romero <santiago_pna@hotmail.com>
2022-12-06 15:21:09 -0300

    santi4246/Back-routes-users

--- api/src/models/UsersModel.js
+++ api/src/models/UsersModel.js
@@ -4,8 +4,9 @@ const { uuid } = require('uuidv4');
 const ProductsSchema = mongoose.Schema({
   _id: {type: String, default: uuid},
   name: {type: String, require: true},
+  email: {type: String, require: true},
   password: {type: String, require: true},
-  phone: {type: Number, require: true},
+  phone: {type: String, require: true},
   address: {type: String, require: true},
   image: {type: String, require: true},
   admin: {type: Boolean, require: true}
