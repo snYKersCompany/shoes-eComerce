e5606c3d62b370720617e98c1afad576e4ac9230
Cristian Treachi <91136333+Cris3h@users.noreply.github.com>
2022-12-07 10:57:56 -0300

    merged

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"; // eslint-disable-next-line
 import { useAuth } from "../../context/authContext";
 //COMPONENTS
-import NavBar from "../NavBar/Navbar";
+import NavBar from "../Navbar/Navbar";
 import Paginated from "../Paged/Paginated.jsx";
 import Filters from "../Filters/Filters.jsx";
 import Footer from "../Footer/Footer";
-import Register from "../Auth/Register";
-import Login from "../Auth/Register";
+import Register from "../Auth/Register"; // eslint-disable-next-line
+import Login from "../Auth/Register"; // eslint-disable-next-line
 
 const Home = () => {
-  const authContext = useAuth();
-  const { user } = useAuth();
+  const authContext = useAuth();// eslint-disable-next-line
+  const { user } = useAuth();// eslint-disable-next-line
 
   return (
     <>
