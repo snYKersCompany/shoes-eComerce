3d7586fed5247731c5c121d274d3890decf01a7a
Santiago L. Romero <santiago_pna@hotmail.com>
2022-12-09 11:43:06 -0300

    santi4246/Back-models-routes-controllers-categories

--- api/src/controllers/Categories/index.js
+++ api/src/controllers/Categories/index.js
@@ -2,16 +2,57 @@ const { CategoryModel } = require('../../models/ModelsDB.js');
 
 const listCategories = async (name) => {
     if (name) {
-        const category = await CategoryModel.find({where: {name: name}});
+        const category = await CategoryModel.findOne({ name: name });
         if (!category) {
             throw new Error(`Could not find the category in the database`);
         }
         return category
     }
-    const categories = await CategoryModel.find();
+    let categories = await CategoryModel.find();
+    categories = categories.map(e => e.name);
     return categories;
 }
 
+const postCategory = async (name) => {
+    const result = await CategoryModel.findOne({name: name});
+    if (result) {
+        throw new Error(`The category ${ name } was already found in the database`);
+    }
+    const category = await CategoryModel.create({ name: name });
+    return `The ${ category.name } was successfully created`;
+}
+
+const deleteCategory = async (name) => {
+    const category = await CategoryModel.findOne({ name: name });
+    if (!category) {
+        return `The category ${ name } was not found in the database`;
+    }
+    await CategoryModel.deleteOne({name: name});
+    return `The category ${ name } was successfully deleted`;
+}
+
+const findCategory = async (name) => {
+    const category = await CategoryModel.findOne({ name: name });
+    if (!category) {
+        throw new Error(`Could not find the category in the database`);        
+    }
+    return category;
+}
+
+const postCategories = async (array) => {    
+    array.map((e) => {
+        CategoryModel.create({
+            name: e.name            
+        })
+    });
+    Promise.all(array);    
+    return `Categories added successfully`;
+}
+
 module.exports = {
-    listCategories
+    listCategories,
+    postCategory,
+    deleteCategory,
+    findCategory,
+    postCategories
 }
--- api/src/models/CategoryModel.js
+++ api/src/models/CategoryModel.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
+const { uuid } = require('uuidv4');
 
 function validate (str) {
     return (/^[ a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/.test(str));
 }
 
 const ProductsSchema = mongoose.Schema({
-    _id: {type: String, require: true},
+    _id: {type: String, require: true, default: uuid},
     name: {type: String, require: true, validate: [validate, 'The field name cannot contain strange characters']},
   });
   const CategoryModel = mongoose.model('category', ProductsSchema);
