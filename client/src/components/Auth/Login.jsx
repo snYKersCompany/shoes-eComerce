5fcce35e4bd80340c08db2692f4f85223e3f7a1f
juaniarrigoni <juani.arrigoni@gmail.comgit config --global user.name juaniarrigonigit initgit config --global user.email juani.arrigoni@gmail.com>
2022-12-12 09:34:36 -0300

    Login and register styles, Google Login Button

--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -6,7 +6,7 @@ import Alert from "./Alert";
 //BS
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import Modal from "react-bootstrap/Modal"; // eslint-disable-line
+import Card from "react-bootstrap/Card";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -25,39 +25,20 @@ const Login = () => {
     setUser({ ...user, [name]: value });
   };
 
-  const handleSubmit = (e) => {
-    return new Promise(() => {
-      e.preventDefault();
-      setError("");
-      logIn(user.email, user.password)
-        .then(() => {
-          navigate("/home");
-        })
-        .catch((error) => {
-          if (error.code === "auth/wrong-password") {
-            setError("Wrong Password");
-          }
-          setError(error.message);
-        });
-    });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await logIn(user.email, user.password);
+      navigate("/home");
+    } catch (error) {
+      console.log(error.code);
+      if (error.code === "auth/invalid-email") {
+        setError("Correo invalido");
+      }
+      setError(error.message);
+    }
   };
-
-  // const handleSubmit = () => {
-  //   const a = new Promise(
-  //     logIn(user.email, user.password).then((user) => {
-  //       alert(`${user} has been loged in snYKers`)
-  //         .then(navigate("home"))
-  //         .catch((error) => {
-  //           console.log(error.code);
-  //           if (error.code === "auth/invalid-email") {
-  //             setError("Correo invalido");
-  //           }
-  //           setError(error.message);
-  //         });
-  //     })
-  //   );
-  //   return a;
-  // };
 
   const handleGoogleLogin = async () => {
     try {
@@ -83,44 +64,53 @@ const Login = () => {
 
   return (
     <>
-      <h1>Login</h1>
-      {error && <Alert message={error} />}
-      <Form onSubmit={(e) => handleSubmit(e)}>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            onChange={(e) => handleChange(e)}
-            name="email"
-            type="email"
-            placeholder="Enter email"
-          />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            onChange={(e) => handleChange(e)}
-            name="password"
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Login
-        </Button>
-        <Button variant="primary" type="submit" onClick={handleResetPassword}>
-          Forgot Password ?
-        </Button>
-      </Form>
-
-      <Button variant="primary" onClick={handleGoogleLogin}>
-        Login with Google
-      </Button>
-      <Link to="/register">
-        <Button variant="primary">Dont have an account? Register here</Button>
-      </Link>
+      <Card className="text-center" style={{ width: "18rem" }}>
+        <Card.Body>
+          <Card.Title>Login</Card.Title>
+          {error && <Alert message={error} />}
+          <Form onSubmit={(e) => handleSubmit(e)}>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                onChange={(e) => handleChange(e)}
+                name="email"
+                type="email"
+                placeholder="Enter email"
+              />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                onChange={(e) => handleChange(e)}
+                name="password"
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Login
+            </Button>
+            <Button
+              variant="primary"
+              type="submit"
+              onClick={handleResetPassword}
+            >
+              Forgot Password ?
+            </Button>
+          </Form>
+          <Button variant="primary" onClick={handleGoogleLogin}>
+            Login with Google
+          </Button>
+          <Link to="/register">
+            <Button variant="primary">
+              Dont have an account? Register here
+            </Button>
+          </Link>
+        </Card.Body>
+      </Card>
     </>
   );
 };
--- client/src/components/Auth/Register.jsx
+++ client/src/components/Auth/Register.jsx
@@ -6,7 +6,7 @@ import Alert from "./Alert";
 //BS
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import Modal from "react-bootstrap/Modal"; // eslint-disable-line
+import Card from "react-bootstrap/Card";
 
 const Register = () => {
   const navigate = useNavigate();
@@ -41,38 +41,44 @@ const Register = () => {
 
   return (
     <>
-      <h1>Register</h1>
-      {error && <Alert message={error} />}
-      <Form onSubmit={(e) => handleSubmit(e)}>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            onChange={(e) => handleChange(e)}
-            name="email"
-            type="email"
-            placeholder="Enter email"
-          />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            onChange={(e) => handleChange(e)}
-            name="password"
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Group>
-        <Form.Group controlId="formBasicCheckbox"></Form.Group>
-        <Button variant="primary" type="submit">
-          Register
-        </Button>
-      </Form>
-      <Link to="/login">
-        <Button variant="primary">Already have an account? Login here</Button>
-      </Link>
+      <Card className="text-center" style={{ width: "18rem" }}>
+        <Card.Body>
+          <Card.Title>Register</Card.Title>
+          {error && <Alert message={error} />}
+          <Form onSubmit={(e) => handleSubmit(e)}>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                onChange={(e) => handleChange(e)}
+                name="email"
+                type="email"
+                placeholder="Enter email"
+              />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                onChange={(e) => handleChange(e)}
+                name="password"
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox"></Form.Group>
+            <Button variant="primary" type="submit">
+              Register
+            </Button>
+          </Form>
+          <Link to="/login">
+            <Button variant="primary">
+              Already have an account? Login here
+            </Button>
+          </Link>
+        </Card.Body>
+      </Card>
     </>
   );
 };
