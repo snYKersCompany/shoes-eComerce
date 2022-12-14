5d8dc14585e2ce33b283b9c2eb3e2f732d4e8043
Your Name <you@example.com>
2022-12-14 20:39:49 -0300

    Componente AdminDashboardUsers

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import products from "./features/products/productsSlice";
 import users from "./features/users/usersSlice"
+import orders from "./features/orders/ordersSlice"
 
 export const store = configureStore({
   reducer: {
     products: products,
-    users: users
+    users: users,
+    orders: orders
   },
 });
