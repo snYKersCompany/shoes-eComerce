cfc2ada4f0dc05525bcadf4af043bbbfd0ba42d3
juaniarrigoni <juani.arrigoni@gmail.comgit config --global user.name juaniarrigonigit initgit config --global user.email juani.arrigoni@gmail.com>
2022-12-16 10:07:57 -0300

    mergin dev

--- client/src/components/VistaIntermedia/VistaIntermedia.jsx
+++ client/src/components/VistaIntermedia/VistaIntermedia.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import CardsContainer from "../CardsContainer/CardsContainer";
 //JSX
-import NavBar from "../NavBar/Navbar";
+import NavBar from "../NavBar/NavBar";
 
-const VistaIntermedia = () => {
+const VistaIntermedia = ({ products }) => {
+  var productsSliced = "hola";
+
   return (
     <>
       <NavBar />
