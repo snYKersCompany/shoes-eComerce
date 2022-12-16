87008d0c6b4be072f8cd6b09ae5cf63db06d7014
Walter Celiz <wceliz.dev@gmail.com>
2022-12-16 11:58:21 -0300

    userFavorites in progress

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,24 +1,22 @@
 import React from "react";
-import NavBar from "../NavBar/Navbar";
-import AdminDashboard from "./AdminDashboard/AdminDashboard"
-import UserDashboard from "./UserDashboard/UserDashboard"
+import NavBar from "../NavBar/NavBar";
+import AdminDashboard from "./AdminDashboard/AdminDashboard";
+import UserDashboard from "./UserDashboard/UserDashboard";
 import "../../styles/userDashboard.css";
 
-
 const Dashboard = () => {
-
-  let user= {role: ['6397b0b5a0730c95052cae5a']}
+  let user = { role: ["6397b0b5a0730c95052cae5a"] };
   return (
     <>
       <NavBar />
-      
-      {user.role[0]=== '6397b0b5a0730c95052cae5a'?
-      <AdminDashboard/> 
-      :
-      <UserDashboard />
-      }
-   </>
-  )     
+
+      {user.role[0] !== "6397b0b5a0730c95052cae5a" ? (
+        <AdminDashboard />
+      ) : (
+        <UserDashboard />
+      )}
+    </>
+  );
 };
 
 export default Dashboard;
--- client/src/components/Dashboard/UserDashboard/UserFavorites.jsx
+++ client/src/components/Dashboard/UserDashboard/UserFavorites.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import Card from "../../CardsContainer/Card";
+import data from "../data.json";
 
 const UserFavorites = () => {
-  return <div>userFavorites</div>;
+  const favs = data;
+  return (
+    <div className="d-flex">
+      <div className="">
+        <h3 className="text-center">Your Favorites</h3>
+        <div className="d-flex">
+          {favs.favorites.map((el) => {
+            return (
+              <Card
+                _id={el._id}
+                name={el.name}
+                price={el.price}
+                card_picture={el.card_picture}
+                brand={el.brand}
+                rating={el.rating}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserFavorites;
