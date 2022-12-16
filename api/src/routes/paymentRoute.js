637167c093f77963bc01e6c586e9d9c878670cb4
MartinGadea12 <martiingadeea1996@gmail.com>
2022-12-16 11:23:13 -0300

    Acces token Paypal

--- api/src/routes/paymentRoute.js
+++ api/src/routes/paymentRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { createOrder, captureOrder, cancelOrder } = require('../controllers/Payments/index');
 
-router.post('/create-order', createOrder)
+router.get('/create-order', createOrder)
 
 router.get('/capture-order', captureOrder)
 
