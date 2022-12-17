92eb8def0bcad2dfe07a8de9f5e50fd440a5e189
Your Name <you@example.com>
2022-12-17 11:16:44 -0300

    funcionalidad del boton favorito

--- client/src/redux/features/users/usersActions.js
+++ client/src/redux/features/users/usersActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { getAllUser, getUserDashboard, updateUserDashboard } from "./usersSlice";
+import { clearUser, getAllUser, getUser, getUserDashboard, updateUserDashboard } from "./usersSlice";
 
 export const findOrCreateUser = (payload) => async () => {
   try {
@@ -21,9 +21,11 @@ export const getAllUsers = () => async (dispatch) => {
 
 export const getUserDashboards = (_id) => async (dispatch) => {
   try {
+    if(!_id) return dispatch(getUserDashboard({}));
     const users = await axios.get(
       `http://localhost:3001/api/users/dashboard/${_id}`
     );
+    console.log("Esto es getUserDashboards",users)
     return dispatch(getUserDashboard(users.data[0]));
   } catch (error) {
     return error;
@@ -32,6 +34,7 @@ export const getUserDashboards = (_id) => async (dispatch) => {
 
 export const addUserProductFavorites = (_id, favorite) => async (dispatch) => {
   try {
+    if(!_id) return
     const users = await axios.put(`http://localhost:3001/api/users/addFavorite/${_id}`, favorite);
     console.log(users.data)
     return dispatch(updateUserDashboard(users.data));
@@ -42,9 +45,26 @@ export const addUserProductFavorites = (_id, favorite) => async (dispatch) => {
 
 export const deleteUserProductFavorites = (_id, favorite) => async (dispatch) => {
   try {
+    if(!_id) return
     const users = await axios.put(`http://localhost:3001/api/users/deleteFavorite/${_id}`, favorite);
     console.log(users.data)
     return dispatch(updateUserDashboard(users.data));
+  } catch (error) {
+    return error;
+  }
+};
+
+export const getUsers = (user) => async (dispatch) => {
+  try {
+    return dispatch(getUser(user));
+  } catch (error) {
+    return error;
+  }
+};
+
+export const clearUsers = () => async (dispatch) => {
+  try {
+    return dispatch(clearUser({}));
   } catch (error) {
     return error;
   }
