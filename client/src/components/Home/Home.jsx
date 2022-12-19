f4b77dc9962469f4ecff9145a0caf5fb9b0adcc0
Your Name <you@example.com>
2022-12-19 12:02:51 -0300

    Modal Vista intermedia Card

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,17 +1,14 @@
 import React, { useEffect } from "react";
 import { useAuth } from "../../context/authContext";
-import { useDispatch } from "react-redux";
 //JSX
 import NavBar from "../NavBar/NavBar";
 import Paginated from "../Paged/Paginated.jsx";
 import Footer from "../Footer/Footer";
 import ScrollToTop from "../ScrollToTop/ScrollToTop";
-import { getUserDashboards } from "../../redux/features/users/usersActions";
 import Carrousel from "../Carrousel";
 //actions
 
 const Home = () => {
-  const dispatch = useDispatch();
 
   //devuelve el user del contexto
   const { user } = useAuth(); // eslint-disable-line
