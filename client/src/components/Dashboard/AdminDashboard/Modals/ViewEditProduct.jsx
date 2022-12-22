625e74b40966ef36508ba42b01763aa11677569c
CrisGab22 <cristhianverarivas22@gmail.com>
2022-12-22 12:13:55 -0500

    save para no perder el progreso

--- client/src/components/Dashboard/AdminDashboard/Modals/ViewEditProduct.jsx
+++ client/src/components/Dashboard/AdminDashboard/Modals/ViewEditProduct.jsx
@@ -2,16 +2,39 @@ import React, { useState } from "react";
 import { VscEdit } from "react-icons/vsc";
 import "../../../../styles/viewEditProduct.css";
 
-const ViewEditProduct = ({ productDetail, setStock, stock }) => {
+const ViewEditProduct = ({ productDetail, setStock, viewStock }) => {
   
+  //Muestra stock o no
   let [control, setControl] = useState(-1) 
 
-  const handleEdit = (e, child) => {
+
+
+  //Deshabilita o habilita la edición de un campo
+  const handleEdit = (e, child, state) => {
     e.preventDefault()
-    console.log(child);
+    console.log(child)
+    switch (child) {
+      case "category":
+        state?
+        setform({...form, categories: ""})
+        :
+        setform({...form, categories: productDetail.categories})
+      
+
+
+      default:
+        break;
+    }
+
     setControl(child)
   }
-
+  
+  
+  
+  const [form, setform] = useState(productDetail)
+  //form tiene esta estructura:
+  //          ↆ
+  //
   // const [form, setform] = useState({
   //   name: "", //string
   //   description: "", //string
@@ -26,6 +49,119 @@ const ViewEditProduct = ({ productDetail, setStock, stock }) => {
   //   img: "", //String
   // });
 
+
+  const [error, setError] = useState({
+    name: " ",
+    description: " ",
+    brand: " ",
+    price: " ",
+    color: " ",
+    gender: " ",
+    size: "Select almost 1 size",
+    categories: "Select almost 1 category",
+    stock: " ",
+    release: " ",
+    img: " ",
+  });
+
+  //eslint-disable-next-line
+  const [controller, setController] = useState({
+    general: false, // cambia la pantalla cuando hace el dispatch para no agregar varias veces el mismo artículo
+    //eslint-disable-next-line
+    name: /[{}<>=@\/\\]/g,
+    //eslint-disable-next-line
+    brand: /[{}<>=@\/\\]/g,
+    price: /^\d*((,||\.)\d)+$/g,
+    sizes: (e) => {
+      let a = [...form.size, e.target.value];
+      return a.length > 1 ? true : false;
+    },
+    radio: true, //hasta que se puedan subir imágenes a la nube
+    release: (date) =>
+      date.split("-")[0] < 1900 || date.split("-")[0] > 2100 ? false : true,
+  });
+
+  //validations  
+  let handleBrand = (e) => {
+    if (
+      e.target.value[e.target.value.length - 2] === " " &&
+      e.target.value[e.target.value.length - 1] === " "
+      ) {
+        return setform({ ...form }); //sin doble espacios
+      }
+      setform({ ...form, brand: e.target.value });
+      controller.brand.test(e.target.value) === false &&
+      controller.brand.test(e.target.value) === false
+      ? setError({ ...error, brand: false })
+      : setError({ ...error, brand: "Invalid character" });
+    };
+    
+    let handleName = (e) => {
+      if (
+        e.target.value[e.target.value.length - 2] === " " &&
+        e.target.value[e.target.value.length - 1] === " "
+      ) {
+        return setform({ ...form }); //sin doble espacios
+      }
+      setform({ ...form, name: e.target.value });
+      controller.name.test(e.target.value) === false &&
+      controller.name.test(e.target.value) === false
+        ? setError({ ...error, name: false })
+        : setError({ ...error, name: "Invalid character" });
+      if (e.target.value.length < 3 || e.target.value.length > 50) {
+        setError({
+          ...error,
+          name: "Must be between 2 and 50 characters in length",
+        });
+      }
+    };
+
+  let handleDescription = (e) => {
+    if (
+      e.target.value[e.target.value.length - 2] === " " &&
+      e.target.value[e.target.value.length - 1] === " "
+      ) {
+        return setform({ ...form }); //sin doble espacios
+    }
+    setform({ ...form, description: e.target.value });
+
+    if (e.target.value.length < 30 || e.target.value.length > 1020) {
+      setError({
+        ...error,
+        description: "Must be between 30 and 1000 characters in length",
+      });
+    }
+    else{
+      setError({
+        ...error,
+        description: false
+      })
+    }
+  };
+
+  let handleGender = (e) => {
+    e.target.value === "none"
+      ? setError({ ...error, gender: "choose a gender" })
+      : setError({ ...error, gender: false });
+    setform({ ...form, gender: [e.target.value] });
+  };
+
+  // let handleCategory = (value) => {
+  //   let a = form.categories;
+  //   a.length >= 1 ? (a = a.filter((el) => el === value)) : (a = []);
+  //   a.length < 1
+  //     ? setform({ ...form, categories: [...form.categories, value] })
+  //     : setform({
+  //         ...form,
+  //         categories: form.categories.filter((el) => el !== value),
+  //       });
+  //   form.categories.length === 1 && value === form.categories[0]
+  //     ? setError({ ...error, categories: "Select almost 1 category" })
+  //     : setError({ ...error, categories: false });
+  // };
+
+
+  
 
 
 
@@ -61,7 +197,7 @@ const ViewEditProduct = ({ productDetail, setStock, stock }) => {
           </fieldset>
         </section>
 
-        {stock ? (
+        {viewStock ? (
           <section className="right d-flex flex-column">
             <div className="d-flex">
               <fieldset className="d-flex flex-column mb-3">
@@ -71,13 +207,15 @@ const ViewEditProduct = ({ productDetail, setStock, stock }) => {
                     className="input"
                     type="text"
                     name="brand"
-                    value={productDetail.brand}
-                    placeholder="nike"
+                    value={form.brand}
+                    onChange={(e) => handleBrand(e)}
+                    placeholder="Insert a Brand"
                   />
                   <button className="ms-1 me-3 input2">
                     <VscEdit className="input3" />
                   </button>
                 </label>
+                <label>{error.brand}</label>
               </fieldset>
 
               <fieldset className="d-flex flex-column mb-3">
@@ -87,13 +225,15 @@ const ViewEditProduct = ({ productDetail, setStock, stock }) => {
                     className="input"
                     type="text"
                     name="name"
-                    value={productDetail.name}
-                    placeholder="jordan"
+                    onChange={(e) => handleName(e)}
+                    value={form.name}
+                    placeholder="Insert a name"
                   />
                   <button className="ms-1 input2">
                     <VscEdit className="input3" />
                   </button>
                 </label>
+                <label>{error.name}</label>
               </fieldset>
             </div>
 
@@ -104,13 +244,15 @@ const ViewEditProduct = ({ productDetail, setStock, stock }) => {
                   className="input"
                   type="text"
                   name="description"
-                  value={productDetail.description}
+                  value={form.description}
+                  onChange={(e)=>handleDescription(e)}
                   placeholder="This Nike Air Jordan 1 Retro High OG &#39;Shadow&#39; 2018 is a retro re-release of an original 1985 colorway. The shoe features a black and medium grey leather upper with a white midsole and black outsole. It also features OG Nike Air branding on the tongue and the Wings logo on the ankle collar. It was last retroed in 2013, and a low-top version dropped in 2015."
                 />
                 <button className="ms-1 input2">
                   <VscEdit className="input3" />
                 </button>
               </label>
+              <label>{error.description}</label>
             </fieldset>
 
             <div className="d-flex">
@@ -121,7 +263,7 @@ const ViewEditProduct = ({ productDetail, setStock, stock }) => {
                     className="input"
                     type="text"
                     name="gender"
-                    value={productDetail.gender}
+                    value={form.gender}
                     placeholder="women"
                   />
                   <button className="ms-1 me-3 input2">
@@ -132,18 +274,22 @@ const ViewEditProduct = ({ productDetail, setStock, stock }) => {
 
               <fieldset className="d-flex flex-column mb-3">
                 Category
+                {form.categories !== ""?
                 <label>
-                  <input
-                    className="input"
-                    type="text"
-                    name="category"
-                    value={productDetail.category}
-                    placeholder="Running"
-                  />
+                  <label>{form.categories}</label>
                   <button className="ms-1 input2">
-                    <VscEdit className="input3" />
+                    <VscEdit className="input3" onClick={(e)=> handleEdit(e, "category", true)} />
                   </button>
                 </label>
+                :
+                <>
+                <select></select>
+                  <button className="ms-1 input2">
+                    <VscEdit className="input3" onClick={(e)=> handleEdit(e, "category", false)} />
+                  </button>
+                </>
+                }
+                
               </fieldset>
             </div>
 
