0df7fdaf23fd02316cc6795d750ecdb9d8c91da3
CrisGab22 <cristhianverarivas22@gmail.com>
2022-12-26 23:32:30 -0500

    update completado

--- client/src/components/Dashboard/AdminDashboard/Modals/ViewEditProduct.jsx
+++ client/src/components/Dashboard/AdminDashboard/Modals/ViewEditProduct.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 import { VscEdit } from "react-icons/vsc";
 import "../../../../styles/viewEditProduct.css";
+import { app } from "../../../../utils/firebase/credentials";
+import { useDispatch } from "react-redux";
+import { putProduct } from "../../../../redux/features/products/productsActions";
 
 
-const ViewEditProduct = ({ productDetail, setStock, viewStock }) => {
-  
+const ViewEditProduct = ({ productDetail, viewStock }) => {
+  const dispatch = useDispatch()
+
+
   //Muestra stock o no
   let [control, setControl] = useState(-1) 
   let [edit, setEdit] = useState("")
@@ -21,8 +26,21 @@ const ViewEditProduct = ({ productDetail, setStock, viewStock }) => {
   }
   
   
-  const [form, setform] = useState(productDetail)
   //form tiene esta estructura:
+  const [form, setform] = useState({
+    brand: productDetail.brand,
+    name: productDetail.name,
+    description: productDetail.description,
+    gender: productDetail.gender,
+    category: productDetail.category,
+    color: productDetail.color,
+    collection: productDetail.collection,
+    release_date: productDetail.release_date,
+    rating: productDetail.rating,
+    price: productDetail.price,
+    img: productDetail.detail_picture,
+    stock: productDetail.stock
+  })
   //          ↆ
   //
   // const [form, setform] = useState({
@@ -39,7 +57,6 @@ const ViewEditProduct = ({ productDetail, setStock, viewStock }) => {
   //   img: "", //String
   // });
 
-  console.log(form)
 
   const [error, setError] = useState({
     name: false,
@@ -53,7 +70,6 @@ const ViewEditProduct = ({ productDetail, setStock, viewStock }) => {
     release: false,
     img: false,
   });
-  console.log(error)
 
   //eslint-disable-next-line
   const [controller, setController] = useState({
@@ -83,7 +99,7 @@ const ViewEditProduct = ({ productDetail, setStock, viewStock }) => {
       if (e.target.value.length < 3 || e.target.value.length > 50) {
         setError({
           ...error,
-          brand: "Must be between 2 and 50 characters in length",
+          brand: "Must be between 3 and 50 characters in length",
         });
       }
     };
@@ -103,32 +119,30 @@ const ViewEditProduct = ({ productDetail, setStock, viewStock }) => {
       if (e.target.value.length < 3 || e.target.value.length > 50) {
         setError({
           ...error,
-          name: "Must be between 2 and 50 characters in length",
+          name: "Must be between 3 and 50 characters in length",
         });
       }
     };
 
-  let handleDescription = (e) => {
+  let handleDescription = (e) =>  {
     if (
       e.target.value[e.target.value.length - 2] === " " &&
       e.target.value[e.target.value.length - 1] === " "
-      ) {
-        return setform({ ...form }); //sin doble espacios
+    ) {
+      return setform({ ...form }); //sin doble espacios
     }
     setform({ ...form, description: e.target.value });
-
+    controller.name.test(e.target.value) === false &&
+    controller.name.test(e.target.value) === false
+      ? setError({ ...error, description: false })
+      : setError({ ...error, description: "Invalid character" });
     if (e.target.value.length < 30 || e.target.value.length > 1020) {
       setError({
         ...error,
-        description: "Must be between 30 and 1000 characters in length",
+        description: "Must be between 30 and 1020 characters in length",
       });
     }
-    else{
-      setError({
-        ...error,
-        description: false
-      })
-    }
+  
   };
 
   let handleGender = (e) => {
@@ -154,15 +168,11 @@ const ViewEditProduct = ({ productDetail, setStock, viewStock }) => {
     setform({...form, detail_picture:e.target.value});
   };
 
-  let handleImgForm = () => {
-    setform({ ...form, img: auxImg });
-    auxImg.length > 1
-      ? setError({ ...error, img: false })
-      : setError({ ...error, img: "Upload a image" });
+  let handleImgForm = (e) => {
+    setform({ ...form, img: e.target.value });
   };
 
   let handleCategory = (e) => {
-    console.log(e.target.value)
    e.target.value === "none"
       ? setError({ ...error, category: "choose a gender" })
       : setError({ ...error, category: false });
@@ -202,7 +212,7 @@ const ViewEditProduct = ({ productDetail, setStock, viewStock }) => {
     controller.name.test(e.target.value) === false
       ? setError({ ...error, collection: false })
       : setError({ ...error, collection: "Invalid character" });
-    if (e.target.value.length < 2 || e.target.value.length > 80) {
+    if (e.target.value.length < 3 || e.target.value.length > 80) {
       setError({
         ...error,
         collection: "Must be between 3 and 80 characters in length",
@@ -218,8 +228,223 @@ const ViewEditProduct = ({ productDetail, setStock, viewStock }) => {
       : setError({ ...error, price: "Invalid response" });
   };
 
-  
+  let handleStock = (e, el) => {
+    el = Number(el)
+    if (el === 3.5) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 3.5: Number(e.target.value) },
+      });
+    }
+    if (el === 4) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 4: Number(e.target.value) },
+      });
+    }
+    if (el === 4.5) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 4.5: Number(e.target.value) },
+      });
+    }
 
+    if (el === 5) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 5: Number(e.target.value) },
+      });
+    }
+
+    if (el === 5.5) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 5.5: Number(e.target.value) },
+      });
+    }
+    
+    if (el === 6) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 6: Number(e.target.value) },
+      });
+    }
+
+    if (el === 6.5) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 6.5: Number(e.target.value) },
+      });
+    }
+
+    if (el === 7) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 7: Number(e.target.value) },
+      });
+    }
+
+    if (el === 7.5) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 7.5: Number(e.target.value) },
+      });
+    }
+
+    if (el === 8) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 8: Number(e.target.value) },
+      });
+    }
+
+    if (el === 8.5) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 8.5: Number(e.target.value) },
+      });
+    }
+
+    if (el === 9) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 9: Number(e.target.value) },
+      });
+    }
+
+    if (el === 9.5) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 9.5: Number(e.target.value) },
+      });
+    }
+
+    if (el === 10) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 10: Number(e.target.value) },
+      });
+    }
+
+    if (el === 10.5) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 10.5: Number(e.target.value) },
+      });
+    }
+
+    if (el === 11) {
+      setform({
+        ...form,
+        stock: { ...form.stock,11: Number(e.target.value) },
+      });
+    }
+    if (el === 11.5) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 11.5: Number(e.target.value) },
+      });
+    }
+
+    if (el === 12) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 12: Number(e.target.value) },
+      });
+    }
+
+    if (el === 12.5) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 12.5: Number(e.target.value) },
+      });
+    }
+    
+    if (el === 13) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 13: Number(e.target.value) },
+      });
+    }
+
+    if (el === 13.5) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 13.5: Number(e.target.value) },
+      });
+    }
+
+    if (el === 14) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 14: Number(e.target.value) },
+      });
+    }
+
+    if (el === 14.5) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 14.5: Number(e.target.value) },
+      });
+    }
+
+    if (el === 15) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 15: Number(e.target.value) },
+      });
+    }
+
+    if (el === 15.5) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 15.5: Number(e.target.value) },
+      });
+    }
+
+    if (el === 16) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 16: Number(e.target.value) },
+      });
+    }
+
+    if (el === 16.5) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 16.5: Number(e.target.value) },
+      });
+    }
+
+    if (el === 17) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 17: Number(e.target.value) },
+      });
+    }
+    if (el === 17.5) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 17.5: Number(e.target.value) },
+      });
+    }
+
+    if (el === 18) {
+      setform({
+        ...form,
+        stock: { ...form.stock, 18: Number(e.target.value) },
+      });
+    }
+    
+    Object.entries(form.stock).map(el => el[1]).filter(el=>el<0).length>0 || Number(e.target.value) < 0?
+    setError({...error, stock: "stock cannot be less than 0"}):
+    setError({...error, stock: false})
+    
+  };
+
+  let handleUpdate = () =>{
+    dispatch(putProduct(productDetail._id,form))
+  }
 
 
   return (
@@ -227,16 +452,37 @@ const ViewEditProduct = ({ productDetail, setStock, viewStock }) => {
       
       <form className="viewEdit d-flex " onSubmit={(e) => e.preventDefault()}>
         <section className="left d-flex flex-column align-items-center justify-content-center">
+          {Object.values(error).filter(el => el !== false).length>0?
+          <button className="d-flex mb-5 p-1" disabled>save</button>
+         :
+          <button className="d-flex mb-5 p-1" onClick={() => handleUpdate(form) }>save</button>
+        }
+         
           <img
             className="imageCard1"
-            src={form.detail_picture}
+            src={form.img}
             alt={form.name}
           />
           <img
             className="imageCard2"
-            src={form.detail_picture}
+            src={form.img}
             alt={form.name}
           />
+
+          {edit!== "img" ?
+          <fieldset className="d-flex flex-column mb-3">
+            <label >
+              Image
+            </label>
+
+          <label className="VEPlabelAux">
+            {form.img.substr(0,27)+"..."} 
+            <button className="input2 ms-1" onClick={(e) => handleEdit(e,"img",false)}>
+              <VscEdit className="input3" />
+            </button>
+          </label>
+        </fieldset>
+          :
           <fieldset className="d-flex flex-column mb-3">
             Image
             <label>
@@ -245,14 +491,15 @@ const ViewEditProduct = ({ productDetail, setStock, viewStock }) => {
                 type="text"
                 name="image"
                 placeholder="Insert URL from your image"
-                value={form.detail_picture}
-                onChange={(e) => handleImg(e)}
-              />
-              <button className="input2 ms-1">
+                value={form.img}
+                onChange={(e) => handleImgForm(e)}
+                />
+              <button className="input2 ms-1" onClick={(e) => handleEdit(e,"",false)}>
                 <VscEdit className="input3" />
               </button>
             </label>
           </fieldset>
+          }
         </section>
 
         {viewStock ? (
@@ -537,31 +784,29 @@ const ViewEditProduct = ({ productDetail, setStock, viewStock }) => {
                   <VscEdit className="input3" />
                 </button>
               </label>
+              <label>{error.price}</label>
             </fieldset>
             } 
           </section>
         ) : (
           <section className="VEPStock d-flex flex-wrap">
-             {productDetail.stock ? (
-              Object.entries(productDetail.stock).map((r) =>
-                  <div className="d-flex containerInputVEP" style={{"order": r[0]*2}}>
-
+             {form.stock ? (
+              Object.entries(form.stock).map((r) =>
+                  <div className="d-flex containerInputVEP" style={{"order": r[0]*2}} key={r[0]}>
                   <fieldset
-                    key={r[0]}
-                    value={r[0]}
-                    
+                    key={r[0]}                    
                     className="d-flex"
                     > 
-                      Size {r[0]}:
                      
+                      Size {r[0]}:
                       {
                         control !== r[0]?
-                        <label>
-                          <label className={"ms-2"} style={{"color": "white"}}>
+                      <label>
+                          <label className={"ms-2 "} style={{"color": "white"}}>
                           {r[1]}
                           </label>
-                          <button className="ms-1 input2">
-                            <VscEdit className="input3" onClick={(e) => handleEdit(e, r[0])}/>
+                          <button className="ms-1 input2" onClick={(e) => handleEdit(e, r[0])}>
+                            <VscEdit className="input3" />
                           </button>
                         </label>
                         :
@@ -570,21 +815,22 @@ const ViewEditProduct = ({ productDetail, setStock, viewStock }) => {
                           type="number"
                           name="ranges"
                           value={r[1]}
+                          onChange = {(e)=> handleStock(e,r[0])}
                           className="input ms-3 inputVEP"
                           />
-                          <button className="ms-1 input2">
-                            <VscEdit className="input3" onClick={(e) => handleEdit(e, -1)}/>
+                          <button className="ms-1 input2"  onClick={(e) => handleEdit(e, "")}>
+                            <VscEdit className="input3" />
                           </button>
                         </label>
                       }
 
-
                   </fieldset>
                   </div>
               )
-            ) : (
-              <></>
-            )}
+              ) : (
+                <></>
+                )}
+                <label style={{"order": 37}}>{error.stock}</label>
           </section>
         )}
       </form>
