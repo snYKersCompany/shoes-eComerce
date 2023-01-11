f05ada1e79e3c5829939db98afb5ab6e63fcabf5
MartinGadea12 <martiingadeea1996@gmail.com>
2023-01-11 14:18:31 -0300

    NoFavorites Modificado

--- client/src/components/Dashboard/UserDashboard/NoFavourites.jsx
+++ client/src/components/Dashboard/UserDashboard/NoFavourites.jsx
@@ -5,11 +5,11 @@ import { IoHeartDislikeSharp } from "react-icons/io5";
 import {Link} from "react-router-dom"
 import "../../../styles/noFavourites.css";
 
-function NoFavourites() {
+function NoFavourites({leyend}) {
   return (
     <div className="fakeBodyNoFav">
 
-          <h3 className="text-center">You need to add products to favorite</h3>
+          <h3 className="text-center">{leyend}</h3>
 
           <Link className="text-decoration-none" to={"/home"}>
             <button className={"btnCard1"}>Let's go!</button>
